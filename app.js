6ece37095026ed7d230f9351efe7796924f79abc
Sammons <bsammnz@gmail.com>
2014-07-05 15:41:28 -0500

    bug in the comments

--- app.js
+++ app.js
@@ -8,24 +8,24 @@ Step 1: fetch information
 
 Step 2: write information to file
 
-The code below executes step 2 before step 1 has completed.
+The code below executes step 2 upon completion of step 1.
 */
 
 
-var request = require( 'request' ); /* for getting something from the internet */
-/* Always have the docs handy for a module! https://github.com/mikeal/request */
-
-var fs      = require( 'fs' );      /* for writing that file */
-/* http://nodejs.org/api/fs.html */
-
-/* who better to scrape than google */
-request( 'http://www.google.com', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-  	
-  	/* write the web content to file */
-	fs.writeFile( 'webcontent.html', body );
-  }
-})
+    var request = require( 'request' ); /* for getting something from the internet */
+    /* Always have the docs handy for a module! https://github.com/mikeal/request */
+    
+    var fs      = require( 'fs' );      /* for writing that file */
+    /* http://nodejs.org/api/fs.html */
+    
+    /* who better to scrape than google */
+    request( 'http://www.google.com', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+      	
+  	    /* write the web content to file */
+	    fs.writeFile( 'webcontent.html', body );
+      }
+    })
 
 
 
