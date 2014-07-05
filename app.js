3f7d3e74595c0522dbf5c570474a113876657088
Sammons <bsammnz@gmail.com>
2014-07-05 16:08:11 -0500

    prep stage 1

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ The code below executes step 2 before step 1 has completed.
 var request = require( 'request' ); /* for getting something from the internet */
 /* Always have the docs handy for a module! https://github.com/mikeal/request */
 
-var fs      = require( 'fs' );      /* for writing that file */
+var fs = require( 'fs' ); /* for writing that file */
 /* http://nodejs.org/api/fs.html */
 
 var scraped_content; /* for storing the web content */
 
-/* who better to scrape than google */
+/* who better to scrape than google? */
 request( 'http://www.google.com', function (error, response, body) {
   if (!error && response.statusCode == 200) {
   	/* store the web content for writing to a file */
