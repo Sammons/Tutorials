3233069fd41743edce15c2c405438c471527b81e
Sammons <bsammnz@gmail.com>
2014-07-28 21:25:28 -0500

    creating pretend async functions

--- app.js
+++ app.js
@@ -1,32 +1,79 @@
 
 /*
-First let's demonstrate the problem.
+First let's discuss a few situations.
 
-Say we want to fetch information from the internet, and then write all of that information to a file.
+Consider:
 
-Step 1: fetch information
+A situation where we want to execute
+many of one asynchronous 
+task before other asynchronous tasks. 
+( many of the same api call )
 
-Step 2: write information to file
+A situation where we want to execute
+many asynchronous tasks
+but for each one to happen only after the 
+previous and we know the order. 
+( sequential api calls for example)
 
-The code below executes step 2 before step 1 has completed.
+To demonstrate the situations, we can create a factory
+which produces "pretend" asynchronous functions.
+Each function will execute in a random bounded period of time
+
 */
 
-var request = require( 'request' ); /* for getting something from the internet */
-/* Always have the docs handy for a module! https://github.com/mikeal/request */
+/*
+ This factory takes a return value, and creates a
+ function which will complete between 0 and 400 ms later
+ and upon completion will execute a callback passed to it.
+ The callback will take an error and the returnvalue -- this
+ function will fail about 30% of the time with a cosmic wave
+ error ( the sun is active today ).
+*/
+function produce_async_function( returnvalue ) {
+	return function( callback ) {
+		setTimeout(
+			function() {
+				var rval = (function() { return returnvalue })()
+				var success = (function() { return Math.random()* 10 > 3 })();
+				if ( success ) callback( null, rval );
+				else callback( {err: 'cosmic waves'}, null )
+		}, ( function() { return Math.random()*400 } )() )
+	}
+}
 
-var fs = require( 'fs' );      /* for writing that file */
-/* http://nodejs.org/api/fs.html */
+/*
+Here is a demonstration of our
+sometimes failing functions in use.
+each iteration of the setInterval
+the counter is ticked, and a new function is 
+generated and executed.
+*/
+var counter = 0
+setInterval(function() {
+	counter++
+	produce_async_function( counter )( function( err, rvalue ){
+		console.log( err, rvalue )
+	} )
+},100)
 
-var scraped_content; /* for storing the web content */
-
-/* who better to scrape than google */
-request( 'http://www.google.com', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-  	/* store the web content for writing to a file */
-  	scraped_content = body;
-  }
-})
-
-/* write the web content to file */
-fs.writeFile( 'webcontent.html', scraped_content );
-
+/* the output looks something like this:
+null 1
+{ err: 'cosmic waves' } null
+null 3
+{ err: 'cosmic waves' } null
+null 5
+null 8
+null 6
+null 7
+null 10
+null 11
+null 9
+{ err: 'cosmic waves' } null
+null 14
+null 13
+{ err: 'cosmic waves' } null
+null 17
+null 16
+{ err: 'cosmic waves' } null
+null 19
+*/
